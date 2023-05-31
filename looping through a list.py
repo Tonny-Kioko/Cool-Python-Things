7ac8ef3e6c6bd7a66cef7cce2f810340c2bf780c
Tonny-Kioko <cfakiokoanthony@gmail.com>
2023-05-31 09:55:45 +0300

    looping through a list

---
+++ looping through a list.py
@@ -0,0 +1,9 @@
+inventory = ['El Caballero', 
+             'RPG-7', 
+             'Supercharger', 
+             '.308 Carbine', 
+             'Tostador',
+               'La Clavadora']
+
+for index, weapon in enumerate(inventory):
+    print (index, weapon)
