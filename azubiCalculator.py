fea5470df1ed4a11b8905240810d7385f0de48e1
Tonny-Kioko <cfakiokoanthony@gmail.com>
2023-06-07 12:39:18 +0300

    azubu calculator

---
+++ azubiCalculator.py
@@ -0,0 +1,12 @@
+foodCharge = float(input("Please enter the charge for the food: "))
+
+tip = foodCharge * 0.18
+
+salesTax = (foodCharge * 0.07)
+
+grandTotal = foodCharge + tip + salesTax
+
+
+print(tip)
+print(salesTax)
+print (grandTotal)
